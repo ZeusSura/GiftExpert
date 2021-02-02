ee37318e01b67501efd77056e1daaf631330ac1e
ZeusSura <Alejandrogzh@gmail.com>
2021-02-02 17:56:24 -0600

    Se cambio index

--- src/components/GridGiftItem.js
+++ src/components/GridGiftItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const GridGiftItem = ({ imagen }) => {
   const { url, title } = imagen;
   return (
-    <div className="animate__animated animate__rotateIn animate__slower animate__infinite">
+    <div className="animate__animated animate__rotateIn animate__slower">
       <div className="card">
         <img src={url} alt={title} />
         <p>{title}</p>
